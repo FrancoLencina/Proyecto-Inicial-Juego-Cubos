5a2f8233acc3f7e4a3485f32390dd8427533e532
Mati Duarte <duarte.lmd28@gmail.com>
2026-08-23 16:38:30 -0300

    Revert "Progreso en los bloques."
    This reverts commit 2f8fce35232cf2dae95babc84260a30ce2c89edc.

--- Juego-Cubos/Assets/Scripts/FruitBlock.cs
+++ Juego-Cubos/Assets/Scripts/FruitBlock.cs
@@ -8,37 +8,9 @@ public class FruitBlock : MonoBehaviour
         
     }
 
-<<<<<<< Updated upstream
     // Update is called once per frame
     void Update()
     {
         
     }
 }
-=======
-    private Renderer blockRenderer;
-
-    public FruitData FruitData => fruitData;
-    public FruitType FruitType => fruitData.FruitType;
-
-    private void Awake()
-    {
-        blockRenderer = GetComponent<Renderer>();
-        ApplyFruitData();
-    }
-
-    public void SetFruitData(FruitData newFruitData)
-    {
-        fruitData = newFruitData;
-        ApplyFruitData();
-    }
-
-    private void ApplyFruitData()
-    {
-        if (fruitData == null || blockRenderer == null)
-            return;
-
-        blockRenderer.material = fruitData.Material;
-    }
-}
->>>>>>> Stashed changes
